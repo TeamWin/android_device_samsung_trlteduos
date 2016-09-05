d419489b1d32adc043e47a7d2b64b597b3a085e7
bigbiff bigbiff <bigbiff@teamw.in>
2016-09-04 20:06:28 -0400

    Initial import of note 4 n9100 tree. Based off J C Adduono's trees.

---
+++ Android.mk
@@ -0,0 +1,3 @@
+ifneq ($(filter trlteduoszh,$(TARGET_DEVICE)),)
+    include $(all-subdir-makefiles)
+endif
---
+++ omni_trlteduoszh.mk
@@ -0,0 +1,12 @@
+# Release name
+PRODUCT_RELEASE_NAME := trlteduoszh
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/trlteduoszh/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := trlteduoszh
+PRODUCT_NAME := omni_trlteduoszh
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := trlteduoszh
+PRODUCT_MANUFACTURER := samsung
